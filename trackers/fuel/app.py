1d80223b6fa773f0bde045e0ea03e99e197325ff
Celeo <celeodor@gmail.com>
2014-12-19 21:40:57 -0800

    Fuel Tracker: Fuel consumption, fuel per hour, and offline time added to display; data loaded via AJAX for cleaner UX Op Tracker: Removed websocket loading

--- trackers/fuel/app.py
+++ trackers/fuel/app.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, session, redirect, url_for, flash
-from datetime import datetime
+from datetime import datetime, timedelta
 import eveapi
 from trackers.shared import app_settings
 from trackers.site.models import System
@@ -26,76 +26,52 @@ fuel_types = {
     16275: 'Strontium Clathrates'
 }
 tower_types = {
-    12235: 'Amarr Control Tower',
-    20059: 'Amarr Control Tower Medium',
-    20060: 'Amarr Control Tower Small',
-    17777: 'Amarr Starbase Control Tower',
-    23761: 'Amarr Starbase Control Tower_LCO',
-    29469: 'Amarr Starbase Control Tower Tough',
-    26732: 'Amarr Starbase Control Tower_Tough_Good Loot',
-    27539: 'Angel Control Tower',
-    27607: 'Angel Control Tower Medium',
-    27610: 'Angel Control Tower Small',
-    27530: 'Blood Control Tower',
-    27589: 'Blood Control Tower Medium',
-    27592: 'Blood Control Tower Small',
-    16213: 'Caldari Control Tower',
-    20061: 'Caldari Control Tower Medium',
-    20062: 'Caldari Control Tower Small',
-    17778: 'Caldari Starbase Control Tower',
-    29507: 'Caldari Starbase Control Tower_LCO',
-    26734: 'Caldari Starbase Control Tower_Tough_Good Loot',
-    27532: 'Dark Blood Control Tower',
-    27591: 'Dark Blood Control Tower Medium',
-    27594: 'Dark Blood Control Tower Small',
-    27540: 'Domination Control Tower',
-    27609: 'Domination Control Tower Medium',
-    27612: 'Domination Control Tower Small',
-    27535: 'Dread Guristas Control Tower',
-    27597: 'Dread Guristas Control Tower Medium',
-    27600: 'Dread Guristas Control Tower Small',
-    12236: 'Gallente Control Tower',
-    20063: 'Gallente Control Tower Medium',
-    20064: 'Gallente Control Tower Small',
-    17779: 'Gallente Starbase Control Tower',
-    29460: 'Gallente Starbase Control Tower_LCO',
-    32194: 'Gallente Starbase Control Tower Tough',
-    26737: 'Gallente Starbase Control Tower_Tough_Good Loot',
-    27533: 'Guristas Control Tower',
-    27595: 'Guristas Control Tower Medium',
-    27598: 'Guristas Control Tower Small',
-    19505: 'Guristas Starbase Control Tower',
-    20326: 'Guristas Starbase Control Tower',
-    25991: 'Guristas Starbase Control Tower_Tough',
-    25375: 'LCO Guristas Control Tower',
-    3059: 'LCO Sansha Starbase Control Tower',
-    3591: 'LCS Angel Control Tower',
-    22719: "Minas Iksan's Control Tower",
-    16214: 'Minmatar Control Tower',
-    20065: 'Minmatar Control Tower Medium',
-    20066: 'Minmatar Control Tower Small',
-    17780: 'Minmatar Starbase Control Tower',
-    22709: 'Minmatar Starbase Control Tower',
-    26729: 'Minmatar Starbase Control Tower_Tough_Good Loot',
-    27290: 'Minmatar Starbase Control Tower_Tough_Good Loot_testing',
-    16286: 'QA Control Tower',
-    4361: 'QA Fuel Control Tower',
-    27780: 'Sansha Control Tower',
-    27782: 'Sansha Control Tower Medium',
-    27784: 'Sansha Control Tower Small',
-    3499: 'Sansha Starbase Control Tower',
-    27536: 'Serpentis Control Tower',
-    27601: 'Serpentis Control Tower Medium',
-    27604: 'Serpentis Control Tower Small',
-    27538: 'Shadow Control Tower',
-    27603: 'Shadow Control Tower Medium',
-    27606: 'Shadow Control Tower Small',
-    3495: 'Shield Transfer Control Tower',
-    22156: 'Sispur Estate Control Tower',
-    17821: 'Slaver Rig Control Tower',
-    27786: 'True Sansha Control Tower',
-    27788: 'True Sansha Control Tower Medium',
-    27790: 'True Sansha Control Tower Small',
+    12235: ['Amarr Control Tower', 40],
+    20059: ['Amarr Control Tower Medium', 20],
+    20060: ['Amarr Control Tower Small', 10],
+    27539: ['Angel Control Tower', 36],
+    27607: ['Angel Control Tower Medium', 18],
+    27610: ['Angel Control Tower Small', 9],
+    27530: ['Blood Control Tower', 36],
+    27589: ['Blood Control Tower Medium', 18],
+    27592: ['Blood Control Tower Small', 9],
+    16213: ['Caldari Control Tower', 40],
+    20061: ['Caldari Control Tower Medium', 20],
+    20062: ['Caldari Control Tower Small', 10],
+    27532: ['Dark Blood Control Tower', 32],
+    27591: ['Dark Blood Control Tower Medium', 16],
+    27594: ['Dark Blood Control Tower Small', 8],
+    27540: ['Domination Control Tower', 36],
+    27609: ['Domination Control Tower Medium', 18],
+    27612: ['Domination Control Tower Small', 9],
+    27535: ['Dread Guristas Control Tower', 32],
+    27597: ['Dread Guristas Control Tower Medium', 16],
+    27600: ['Dread Guristas Control Tower Small', 8],
+    12236: ['Gallente Control Tower', 40],
+    20063: ['Gallente Control Tower Medium', 20],
+    20064: ['Gallente Control Tower Small', 10],
+    27533: ['Guristas Control Tower', 36],
+    27595: ['Guristas Control Tower Medium', 18],
+    27598: ['Guristas Control Tower Small', 9],
+    25375: ['LCO Guristas Control Tower', 36],
+    22719: ['Minas Iksan\'s Control Tower', 36],
+    16214: ['Minmatar Control Tower', 40],
+    20065: ['Minmatar Control Tower Medium', 20],
+    20066: ['Minmatar Control Tower Small', 10],
+    27780: ['Sansha Control Tower', 36],
+    27782: ['Sansha Control Tower Medium', 18],
+    27784: ['Sansha Control Tower Small', 9],
+    27536: ['Serpentis Control Tower', 36],
+    27601: ['Serpentis Control Tower Medium', 18],
+    27604: ['Serpentis Control Tower Small', 9],
+    27538: ['Shadow Control Tower', 36],
+    27603: ['Shadow Control Tower Medium', 18],
+    27606: ['Shadow Control Tower Small', 9],
+    22156: ['Sispur Estate Control Tower', 36],
+    17821: ['Slaver Rig Control Tower', 36],
+    27786: ['True Sansha Control Tower', 36],
+    27788: ['True Sansha Control Tower Medium', 18],
+    27790: ['True Sansha Control Tower Small', 9],
 }
 
 
@@ -112,12 +88,22 @@ def _prerender():
     return dict(displayname=_name(), now=datetime.utcnow())
 
 
-@blueprint.route('/')
-def index():
-    """ View: index page """
+@blueprint.before_request
+def _preprocess():
     if not _name() in app_settings['LEADERSHIP']:
         flash('You cannot view this page', 'danger')
         return redirect(url_for('site_tracker.index'))
+
+
+@blueprint.route('/')
+def index():
+    """ View: index page """
+    return render_template('fuel/index.html')
+
+
+@blueprint.route('/data')
+def data():
+    """ AJAX View: Load tower data into the index page """
     info = []
     for pair in pairs:
         auth = api.auth(keyID=pair[0], vCode=pair[1])
@@ -129,7 +115,7 @@ def index():
                 'Owner': key_details.characters[0].corporationName,
                 'API Tower ID': pos_basic.itemID,
                 'Tower Item Type ID': pos_basic.typeID,
-                'Tower Item Type Name': tower_types[pos_basic.typeID],
+                'Tower Item Type Name': tower_types[pos_basic.typeID][0],
                 'State': tower_status[pos_basic.state],
                 'Location System ID': System.query.filter_by(map_id=pos_basic.locationID).first().name,
                 'Location Moon ID': pos_basic.moonID,
@@ -141,7 +127,10 @@ def index():
                 'Attack if security status lower than': pos_details.combatSettings.onStatusDrop.standing if not pos_details.combatSettings.onStatusDrop.enabled == '0' else 'Disabled',
                 'Attack if other security status is dropping': bool(pos_details.combatSettings.onAggression),
                 'Attack if at war': bool(pos_details.combatSettings.onCorporationWar),
+                'Fuel Used Per Hour': tower_types[pos_basic.typeID][1],
+                'Hours Left': int([fuel.quantity for fuel in pos_details.fuel if not fuel.typeID == 16275][0] / tower_types[pos_basic.typeID][1]) if len([fuel.quantity for fuel in pos_details.fuel if not fuel.typeID == 16275]) > 0 else 0,
+                'Offline On': datetime.utcnow() + timedelta(int([fuel.quantity for fuel in pos_details.fuel if not fuel.typeID == 16275][0] / tower_types[pos_basic.typeID][1])) if len([fuel.quantity for fuel in pos_details.fuel if not fuel.typeID == 16275]) > 0 else 'Offline now',
             }
             poses.append(data)
         info.extend(poses)
-    return render_template('fuel/index.html', info=info)
+    return render_template('fuel/data.html', info=info)
